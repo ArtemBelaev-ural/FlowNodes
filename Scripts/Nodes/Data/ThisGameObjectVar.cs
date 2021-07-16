f669881e3f1ba4d8986f6b7a82fabf2559b7fb3d
ArtemBelaev-ural <belyaev.a@ural-games.ru>
2021-07-16 16:45:57 +0500

    New nodes: GetTransform, ThisGameObject

---
+++ Scripts/Nodes/Data/ThisGameObjectVar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace HalfBlind.Nodes {
+    [CreateNodeMenu("GameObject/ThisGameObject")]
+    [NodeWidth(140)]
+    public class ThisGameObject : MonoNode 
+    {
+        [Output] public GameObject output;
+
+        private void Reset()
+        {
+            Name = "ThisGameObject";
+        }
+
+        public override object GetValue(NodePort port) 
+        {
+            if (port.fieldName == nameof(output)) 
+            {
+                return gameObject;
+            }
+            return null; // Replace this
+        }
+    }
+}
