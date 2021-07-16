c6364507eaf4fc1448565b426e388aa0c36a6f90
ArtemBelaev-ural <belyaev.a@ural-games.ru>
2021-07-16 19:06:26 +0500

    Add node: LogObjectNode

---
+++ Scripts/Nodes/Actions/Unity/LogObjectNode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace BoGD {
+    [CreateNodeMenu("Utils/"+nameof(LogObjectNode))]
+    public class LogObjectNode : FlowNode 
+    {
+        [Input(typeConstraint: TypeConstraint.None)] public string inputAsObject;
+
+        public override void ExecuteNode() 
+        {
+            object[] objects = GetPort(nameof(inputAsObject)).GetInputValues();
+            foreach (object obj in objects)
+            {
+                Debug.Log(obj);
+            }
+        }
+
+        public override object GetValue(NodePort port) {
+            return null;
+        }
+    }
+}
