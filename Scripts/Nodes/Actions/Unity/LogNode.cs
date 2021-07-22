94d42aea843cdf47fb7e089ce643d2c18b048f8d
ArtemBelaev-ural <belyaev.a@ural-games.ru>
2021-07-22 19:21:08 +0500

    Multiline LogNode

--- Scripts/Nodes/Actions/Unity/LogNode.cs
+++ Scripts/Nodes/Actions/Unity/LogNode.cs
@@ -6,11 +6,27 @@ namespace HalfBlind.Nodes {
     public class LogNode : FlowNode {
         [Input] public string Text;
 
-        public override void ExecuteNode() {
-            Debug.Log(GetInputValue<object>(nameof(Text), Text));
+        public override void ExecuteNode() 
+        {
+            NodePort port = GetInputPort(nameof(Text));
+            int count = port.ConnectionCount;
+            if (count == 1)
+            {
+                Debug.LogFormat("<color=brown>{0}: </color>" + port.GetInputValue<object>(), Name);
+            }
+            else
+            {
+                Debug.LogFormat("<color=brown>{0} ({1} inputs):</color>", Name, count);
+                for (int i = 0; i < count; ++i)
+                {
+                    object[] input = port.GetInputValues();
+                    Debug.LogFormat("<color=brown>{0}) </color>" + input[i], i);
+                }
+            }
         }
 
-        public override object GetValue(NodePort port) {
+        public override object GetValue(NodePort port) 
+        {
             return null;
         }
     }
